2a72a23fcdf2013afc5d2928646c6a797e638b00
Guest <guest-Pcn9Kp@meet.mit.edu>
2014-02-20 20:01:32 +0200

    lab4

---
+++ Lab4/number .py
@@ -0,0 +1,26 @@
+class Integer(number):
+        def __init__(self, number):
+                self.number=number
+ 
+     def Display(self, number):
+                   return "This is a positive number!"
+                          
+           elif self.type<0:
+                   return "This is a negative number!"
+           else:
+                   return "Zero is not a positive or a negative number!"
+
+    def__init__(self, number):
+         if self.number>0
+if __name__==" main ":
+    print "Michele"
+    print ("Enter a number!")
+    
+    
+
+class NegativeInteger(Integer):
+        def __init__(self, number)
+
+
+
+
